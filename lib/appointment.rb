0e1ff8f3f2814ecaf461bda13b91f955436d0ae6
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-08 19:12:38 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,16 +1,16 @@
-class Apointment
-  
-  attr_accessor :date, :patient, :doctor 
+class Appointment
+  attr_accessor :date, :doctor, :patient
+
   @@all = []
-  
+
   def initialize(date, patient, doctor)
-    @date = "Monday, August 1st"
-    @patient = patient 
-    @doctor = doctor 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-end 
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
